2dd61d3719a74707ac7e181ace8c2a2e792929bd
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2024-04-19 17:05:38 -0700

    no-op
    PiperOrigin-RevId: 626505302

--- tfx/orchestration/experimental/core/mlmd_state_test.py
+++ tfx/orchestration/experimental/core/mlmd_state_test.py
@@ -120,6 +120,20 @@ class MlmdStateTest(test_utils.TfxTest):
     event_on_commit = threading.Event()
     got_pre_commit_execution = None
     got_post_commit_execution = None
+    last_known_state_changed = None
+    got_original_execution = None
+    got_modified_execution = None
+
+    def pre_commit(original_execution, modified_execution):
+      nonlocal last_known_state_changed
+      nonlocal got_original_execution
+      nonlocal got_modified_execution
+      got_original_execution = original_execution
+      got_modified_execution = modified_execution
+      last_known_state_changed = (
+          modified_execution.last_known_state
+          != original_execution.last_known_state
+      )
 
     def on_commit(pre_commit_execution, post_commit_execution):
       nonlocal got_pre_commit_execution
@@ -132,7 +146,8 @@ class MlmdStateTest(test_utils.TfxTest):
       expected_execution = _write_test_execution(m)
       # Mutate execution.
       with mlmd_state.mlmd_execution_atomic_op(
-          m, expected_execution.id, on_commit=on_commit) as execution:
+          m, expected_execution.id, on_commit=on_commit, pre_commit=pre_commit
+      ) as execution:
         self.assertEqual(expected_execution, execution)
         execution.last_known_state = metadata_store_pb2.Execution.CANCELED
         self.assertFalse(event_on_commit.is_set())  # not yet invoked.
@@ -160,6 +175,19 @@ class MlmdStateTest(test_utils.TfxTest):
       with mlmd_state.mlmd_execution_atomic_op(
           m, expected_execution.id) as execution2:
         self.assertEqual(execution, execution2)
+
+      # Test that the diff flag is properly populated.
+      self.assertTrue(last_known_state_changed)
+      # Test that we made a deep copy of the executions, so mutating them
+      # doesn't mutate the values in the cache.
+      got_original_execution.last_known_state = (
+          metadata_store_pb2.Execution.UNKNOWN)
+      got_modified_execution.last_known_state = (
+          metadata_store_pb2.Execution.UNKNOWN)
+      self.assertEqual(
+          mlmd_state._execution_cache._cache[execution.id].last_known_state,
+          metadata_store_pb2.Execution.CANCELED,
+      )
 
   def test_mlmd_execution_absent(self):
     with self._mlmd_connection as m:
