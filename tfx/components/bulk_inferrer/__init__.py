460e0316d40afdb883bcef66f981239e538411b8
Madhur Karampudi <142544288+vkarampudi@users.noreply.github.com>
2024-11-15 16:01:33 -0800

    Update __init__.py

--- tfx/components/bulk_inferrer/__init__.py
+++ tfx/components/bulk_inferrer/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from tfx.components.bulk_inferrer import component
+from tfx.components.bulk_inferrer.component import BulkInferrer
 
 __all__ = [
     "BulkInferrer"
