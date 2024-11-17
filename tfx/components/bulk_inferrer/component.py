6cbc261087043e16e1168be547e2a20b67035a9b
Madhur Karampudi <142544288+vkarampudi@users.noreply.github.com>
2024-11-16 19:08:41 -0800

    Update component.py

--- tfx/components/bulk_inferrer/component.py
+++ tfx/components/bulk_inferrer/component.py
@@ -23,10 +23,8 @@ from tfx.orchestration import data_types
 from tfx.proto import bulk_inferrer_pb2
 from tfx.types import standard_artifacts
 from tfx.types import standard_component_specs
-import pytest
 
 
-@pytest.fixture
 class BulkInferrer(base_beam_component.BaseBeamComponent):
   """A TFX component to do batch inference on a model with unlabelled examples.
 
