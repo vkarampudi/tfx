a59299deff3a4762f57a6273ac758b8e3bac603b
Madhur Karampudi <142544288+vkarampudi@users.noreply.github.com>
2024-11-16 20:22:37 -0800

    Update features.py

--- tfx/examples/ranking/features.py
+++ tfx/examples/ranking/features.py
@@ -17,37 +17,37 @@
 These names will be shared between the transform and the model.
 """
 
-"""import tensorflow as tf
-from tfx.examples.ranking import struct2tensor_parsing_utils
+# import tensorflow as tf
+# from tfx.examples.ranking import struct2tensor_parsing_utils
 
-# Labels are expected to be dense. In case of a batch of ELWCs have different
-# number of documents, the shape of the label is [N, D], where N is the batch
-# size, D is the maximum number of documents in the batch. If an ELWC in the
-# batch has D_0 < D documents, then the value of label at D0 <= d < D must be
-# negative to indicate that the document is invalid.
-LABEL_PADDING_VALUE = -1
-
-# Names of features in the ELWC.
+# Constants defining the label padding value and feature names
+"""LABEL_PADDING_VALUE = -1
 QUERY_TOKENS = 'query_tokens'
 DOCUMENT_TOKENS = 'document_tokens'
 LABEL = 'relevance'
+LIST_SIZE_FEATURE_NAME = 'example_list_size'"""
 
-# This "feature" does not exist in the data but will be created on the fly.
-LIST_SIZE_FEATURE_NAME = 'example_list_size'
+# def get_features():
+  """Defines the context features, example features, and label specifications for parsing.
 
+  Returns:
+    A tuple of three lists:
+      - context_features: A list of features representing the context information.
+      - example_features: A list of features representing the example information.
+      - label: A feature representing the label.
+  """
 
-def get_features():
-
-  # Defines the context features and example features spec for parsing.
-
-  context_features = [
+  # Define context features
+  """context_features = [
       struct2tensor_parsing_utils.Feature(QUERY_TOKENS, tf.string)
   ]
 
+  # Define example features
   example_features = [
       struct2tensor_parsing_utils.Feature(DOCUMENT_TOKENS, tf.string)
   ]
 
+  # Define the label feature
   label = struct2tensor_parsing_utils.Feature(LABEL, tf.int64)
 
   return context_features, example_features, label"""
