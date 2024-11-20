0dbb34a9abe0d269b21b75025e662140fa75b0bc
Madhur Karampudi <142544288+vkarampudi@users.noreply.github.com>
2024-11-20 13:46:13 -0800

    Update ranking_pipeline_e2e_test.py

--- tfx/examples/ranking/ranking_pipeline_e2e_test.py
+++ tfx/examples/ranking/ranking_pipeline_e2e_test.py
@@ -65,6 +65,7 @@ class RankingPipelineTest(tf.test.TestCase):
       execution = tf.io.gfile.listdir(os.path.join(component_path, output))
       self.assertEqual(1, len(execution))
 
+  @pytest.mark.xfail(run=False, reason="This is due to TF Ranking not supporting TensorFlow 2.16, We should re-enable it when support is added."
   def testPipeline(self):
     BeamDagRunner().run(
         ranking_pipeline._create_pipeline(
