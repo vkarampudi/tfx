9ca221d150c17684267ac6913f3c5139796afde7
Madhur Karampudi <142544288+vkarampudi@users.noreply.github.com>
2024-11-20 16:11:19 -0800

    Update executor.py

--- tfx/components/evaluator/executor.py
+++ tfx/components/evaluator/executor.py
@@ -19,8 +19,9 @@ from typing import Any, Dict, List
 from absl import logging
 import apache_beam as beam
 import tensorflow_model_analysis as tfma
-# The addons API is based on the estimator, we no longer supporting any estimator related APIs, So removing TFMA addons and post_export_metrics.
-# import tensorflow_model_analysis.addons.fairness.post_export_metrics.fairness_indicators  # pylint: disable=unused-import
+# Need to import the following module so that the fairness indicator post-export
+# metric is registered.
+import tensorflow_model_analysis.addons.fairness.post_export_metrics.fairness_indicators  # pylint: disable=unused-import
 from tfx import types
 from tfx.components.evaluator import constants
 from tfx.components.util import udf_utils
@@ -101,7 +102,17 @@ class Executor(base_beam_executor.BaseBeamExecutor):
 
     self._log_startup(input_dict, output_dict, exec_properties)
 
-           
+    # Add fairness indicator metric callback if necessary.
+    fairness_indicator_thresholds = json_utils.loads(
+        exec_properties.get(
+            standard_component_specs.FAIRNESS_INDICATOR_THRESHOLDS_KEY, 'null'))
+    add_metrics_callbacks = None
+    if fairness_indicator_thresholds:
+      add_metrics_callbacks = [
+          tfma.post_export_metrics.fairness_indicators(  # pytype: disable=module-attr
+              thresholds=fairness_indicator_thresholds),
+      ]
+
     output_uri = artifact_utils.get_single_uri(
         output_dict[constants.EVALUATION_KEY])
 
@@ -185,7 +196,7 @@ class Executor(base_beam_executor.BaseBeamExecutor):
                 eval_saved_model_path=model_path,
                 model_name=model_spec.name,
                 eval_config=eval_config,
-                add_metrics_callbacks=None))
+                add_metrics_callbacks=add_metrics_callbacks))
     else:
       eval_config = None
       assert (standard_component_specs.FEATURE_SLICING_SPEC_KEY
@@ -208,7 +219,7 @@ class Executor(base_beam_executor.BaseBeamExecutor):
               eval_saved_model_path=model_path,
               model_name='',
               eval_config=None,
-              add_metrics_callbacks=None))
+              add_metrics_callbacks=add_metrics_callbacks))
 
     eval_shared_model = models[0] if len(models) == 1 else models
     schema = None
